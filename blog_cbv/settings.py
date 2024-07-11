a8e7eb3e02363f3e0ebd9fd78a80cd593d8b298c
yegerless <Sergey.sys@mail.ru>
2024-07-11 13:18:50 +0300

    install django-mptt-admin

--- blog_cbv/settings.py
+++ blog_cbv/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.blog.apps.BlogConfig',
-    'mptt'
+    'mptt',
+    'django-mptt-admin'
 ]
 
 MIDDLEWARE = [
