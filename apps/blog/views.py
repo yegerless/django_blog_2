df7f0071e2cc1f6b3767161fb9b7cb020d5ecd7b
yegerless <Sergey.sys@mail.ru>
2024-07-16 15:39:50 +0300

    add dinamic title in main.html template

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
@@ -8,3 +9,9 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
