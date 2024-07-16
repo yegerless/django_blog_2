162a3dea78ea6edac1974c514c6ed27f13e5f60f
yegerless <Sergey.sys@mail.ru>
2024-07-16 15:18:06 +0300

    create PostListView for blog app

---
+++ apps/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostListView
+
+
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='home')
+]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    contaxt_object_name = 'posts'
